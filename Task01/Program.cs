99518d5e2171026064dd1991bd15ff5d6c88489a
Wall-e <you@mail.com>
2023-09-23 00:18:15 +0300

    reshenie zadachi_1

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿int x, y;
+Console.Write("Введите число x: ");
+while (!int.TryParse(Console.ReadLine(), out x))
+    Console.WriteLine("Введите целое число");
+Console.Write("Введите число y: ");
+while (!int.TryParse(Console.ReadLine(), out y))
+    Console.WriteLine("Введите целое число");
+if (y/x == x)
+{
+    Console.WriteLine($"x = {x}, y = {y} -> Да");
+}
+else
+{
+    Console.WriteLine($"x = {x}, y = {y} -> Нет");
+}                         
