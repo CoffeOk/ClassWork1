9603f4066de4a8b5ac3840e58256dd1106c644b4
Wall-e <you@mail.com>
2023-09-23 01:52:57 +0300

    reshenie zadachi_7

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿// На входе одно трёхзначное число
+// На выходе - последняя цифра числа
+
+int x, y;
+Control:
+Console.Write ("Введите целое число: ");
+while(!int.TryParse(Console.ReadLine(), out x))
+Console.Write("Данные не корректны, повторите ввод: ");
+if (x < 1000)
+{
+    y = x % 10;
+    Console.Write($"{x} -> {y}");
+}
+else
+{
+    Console.WriteLine("Введите число < 1000");
+    goto Control;
+}
