5877f5311acd0ccf2134a71f8162388a358c499b
Wall-e <you@mail.com>
2023-09-23 01:20:41 +0300

    reshenie zadachi_5

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//На вход принимает число
+//На выходе числа от -n до n
+
+int n, otrn;
+Console.Write("Введите целое число: ");
+while(!int.TryParse(Console.ReadLine(), out n))
+Console.Write("Данные не корректны, повторите ввод: ");
+otrn = -n;
+while (otrn <= n)
+{
+    Console.Write($"{otrn++} ");//Console.Write($"{otrn} ");
+    //otrn ++;
+}
