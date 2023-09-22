044cea15286bb27f70788f7d25296e5967305ba7
Wall-e <you@mail.com>
2023-09-23 02:34:59 +0300

    Reshenie zadachi_7

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -6,13 +6,18 @@ Control:
 Console.Write ("Введите целое число: ");
 while(!int.TryParse(Console.ReadLine(), out x))
 Console.Write("Данные не корректны, повторите ввод: ");
-if (x < 1000)
+if (x < 0 && x < -100 && x > -1000)
+{
+    y = x % 10;
+    Console.Write($"{x} -> {y}");
+}
+else if (x > 0 && x >= 100 && x < 1000)
 {
     y = x % 10;
     Console.Write($"{x} -> {y}");
 }
 else
 {
-    Console.WriteLine("Введите число < 1000");
+    Console.WriteLine("Введите трёхзначное число");
     goto Control;
 }
